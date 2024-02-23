d13845a3774a5e5bd713c28cd044911d509b8430
NMilivojevic <nevmilivojevic@gmail.com>
2024-02-23 23:18:13 +0100

    parse

--- server/services/contractService.js
+++ server/services/contractService.js
@@ -9,17 +9,28 @@ const contract = new web3.eth.Contract(TestContract.abi, contractAddress);
 
 const getBalance = async (account) => {
     const balance = await contract.methods.balanceOf(account).call();
-    return (parseFloat(balance) / 10 ** 18).toLocaleString();
+    const parsedBalance = parseFloat(balance) / 10 ** 18;
+
+    // Define a tolerance for the comparison
+    const tolerance = 1e-10;
+
+    // Check if the absolute difference between the parsed balance and 1e-7 is within the tolerance
+    if (Math.abs(parsedBalance - 1e-7) < tolerance) {
+        return 0;
+    }
+
+    // Otherwise, return the parsed balance
+    return parsedBalance;
 };
 
 const getMinStakeAmount = async () => {
     const minStakeAmount = await contract.methods.minStakeAmount().call();
-    return (parseFloat(minStakeAmount) / 10 ** 18).toLocaleString();
+    return parseFloat(minStakeAmount) / 10 ** 18;
 };
 
 const getTotalSupply = async () => {
     const totalSupply = await contract.methods.totalSupply().call();
-    return (parseFloat(totalSupply) / 10 ** 18).toLocaleString();
+    return parseFloat(totalSupply) / 10 ** 18;
 };
 
 module.exports = { getBalance, getMinStakeAmount, getTotalSupply };
